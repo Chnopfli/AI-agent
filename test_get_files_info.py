3ea258b83a48f0c3d07ff2117a2d3e170623dae9
Mauro B <mauroalessandrobuehler@gmail.com>
2026-09-14 14:23:54 +0200

    added get_files_info

---
+++ test_get_files_info.py
@@ -0,0 +1,6 @@
+from functions.get_files_info import get_files_info
+
+print(get_files_info("calculator", ".") + "\n" + 
+      get_files_info("calculator", "/bin") + "\n" + 
+      get_files_info("calculator", "../") + "\n" + 
+      get_files_info("calculator", "main.py"))
