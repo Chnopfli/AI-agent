b4d44f42ff729b1e49dc05198282a229770133e8
Mauro B <mauroalessandrobuehler@gmail.com>
2026-09-15 15:50:45 +0200

    prettier logs in get_files_test

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -2,10 +2,10 @@ from functions.get_files_info import get_files_info
 
 print("\n" +
     "Result for current directory:" + "\n" +
-      get_files_info("calculator", ".") + "\n" + "\n" +
+      get_files_info("calculator", ".") + "\n\n" +
       "Result for 'pkg' directory:" + "\n" +
-      get_files_info("calculator", "pkg") + "\n" + "\n" +
+      get_files_info("calculator", "pkg") + "\n\n" +
       "Result for '/bin' directory:" + "\n" +
-      get_files_info("calculator", "/bin") + "\n" + "\n" +
+      get_files_info("calculator", "/bin") + "\n\n" +
       "Result for '../' directory:" + "\n" + 
       get_files_info("calculator", "../"))
