3cfc18e27e99b06e1f78e8f8f936d3a80ec5b3d6
Mauro B <mauroalessandrobuehler@gmail.com>
2026-09-16 08:43:47 +0200

    more testcases for test_get_file_content

--- test_get_file_content.py
+++ test_get_file_content.py
@@ -10,7 +10,23 @@ from functions.get_file_content import get_file_content
       "Result for '../' directory:" + "\n" + 
       get_files_info("calculator", "../"))
       '''
+'''
 result = get_file_content("calculator", "lorem.txt")
 print(f"lorem.txt length: {len(result)}")
 print(f"lorem.txt truncated: {'truncated' in result}")
 print()
+'''
+
+testcases: list[tuple] = [
+("calculator", "main.py"),
+("calculator", "pkg/calculator.py"),
+("calculator", "/bin/cat"), #(this should return an error string)
+("calculator", "pkg/does_not_exist.py") #(this should return an error string)
+]
+for case in testcases:
+    result = get_file_content(case[0], case[1])
+    if "Error: File not found" in result or "Error: Cannot read" in result:
+        print(result) 
+    else:
+       print(f"{case[1]} length: {len(result)}")
+       print(f"{case[1]} truncated: {'truncated' in result}")
